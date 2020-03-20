4947caef0a64cae3284b49f7323bde2eb73b93fa
Daniele Giaquinto <d.giaquinto2@studenti.unisa.it>
2020-03-20 12:42:28 +0100

    Es3 Implemented

--- Array/esercizio_3/insertionsort.c
+++ Array/esercizio_3/insertionsort.c
@@ -2,52 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-int cerca_posizione(int a[], int n, int elem)
-{ 
-//    TODO
-    return 1;
+int cerca_posizione(char a[], int n, int elem){
+    int p;  // posizione da restituire
+    int i = n-1; 
+
+    while(i > 0 && a[i] > elem) 
+        i--;
+
+    if(a[i] <= elem)
+            p = i+1; 
+    else p = i;  //  usciti dal ciclo con i = 0 - elem è l’elemento minore
+
+    return p; 
 }
 
-void shift_destra(int a[], int h, int k) 
+void shift_destra(char a[], int h, int k) 
 { 
-    while (h >= 0 && a[h] > k) 
-    {  
-        a[h + 1] = a[h];  
-        h = h - 1;  
-    }  
-    a[h + 1] = k; 
+    for (int i = k; i >= h; i--) 
+        a[i+1] = a[i];  
 }
 
-void insertion_sort(int a[], int n) 
+void insertion_sort(char a[], int n) 
 { 
-    int i;
-    for (i = 1; i < n; i++) 
-    {  
-        /* Move elements of a[0..i-1], that are  
-        greater than key, to one position ahead  
-        of their current position */
-        shift_destra(a, i-1, a[i]);
-    }  
+    int i, p, next;           
+
+    for (i = 1; i < n; i++) {
+            next = a[i];
+
+            p = cerca_posizione(a, i, next);
+            shift_destra(a, p, i-1);
+
+            a[p] = next;  
+    }
+}
+
+char* get_surname()
+{
+    char buffer[64];
+    printf("Inserisci il tuo cognome \n");
+    fgets(buffer, sizeof buffer, stdin);
+    int strLength = strlen(buffer);
+    if(strLength == 0) {
+        printf("\"Cognome\" non inserito.\n");
+        exit(1);
+    } else {
+        char *strInHeap = (char *)malloc((strLength + 1) * sizeof(char));
+        strcpy(strInHeap, buffer);
+        return strInHeap;
+    }
 }
 
 
 int main (void){
     char* cognome; 
-    int dim = 0;
-    char buffer[64];
-    
-    printf("Inserisci il tuo cognome \n");
-    fgets(buffer, sizeof buffer, stdin);
-    
-    dim = (unsigned)strlen(buffer);
+    cognome = get_surname();
+    int dim = (unsigned)strlen(cognome);
+
     for (int i=0; i<dim; i++){
-        printf("%c ", buffer[i]);
+        printf("%c ", cognome[i]);
     }    
-    //c'e' un problema qui come passare
-    insertion_sort((int *)buffer, dim);
+
+    insertion_sort(&cognome[0], dim);
     
     for (int i=0; i<dim; i++){
-        printf("%c ", buffer[i]);
+        printf("%c ", cognome[i]);
     }                      
         
     printf("\n");
